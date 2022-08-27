39a4c56b3b224707596fe233a3d8d7d6b103ba1b
yk Wei <ykwei7@gmail.com>
2022-08-27 15:45:01 +0800

    Add conditional styling when inputs are empty

--- frontend/src/views/SignupPage/SignupPage.js
+++ frontend/src/views/SignupPage/SignupPage.js
@@ -11,7 +11,7 @@ import {
 } from "@mui/material";
 import { useState } from "react";
 import axios from "axios";
-import { URL_USER_SVC } from "configs";
+import { URL_USER_SVC, HEROKU_ENDPOINT } from "configs";
 import { STATUS_CODE_CONFLICT, STATUS_CODE_CREATED } from "constants";
 import { Link } from "react-router-dom";
 import "./signup.scss";
@@ -20,22 +20,29 @@ import mainLogo from "assets/logo.png";
 function SignupPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [isUserInputTouched, setUserInputTouched] = useState(false);
+  const [isEmailInputTouched, setEmailInputTouched] = useState(false);
+  const [isPasswordInputTouched, setPasswordInputTouched] = useState(false);
   const [dialogTitle, setDialogTitle] = useState("");
   const [dialogMsg, setDialogMsg] = useState("");
   const [isSignupSuccess, setIsSignupSuccess] = useState(false);
 
   const handleSignup = async () => {
     setIsSignupSuccess(false);
+    const postUserEndpoint = HEROKU_ENDPOINT + "signup";
     const res = await axios
-      .post(URL_USER_SVC, { username, password })
+      .post(postUserEndpoint, { name: username, password, email })
       .catch((err) => {
+        console.log(err);
         if (err.response.status === STATUS_CODE_CONFLICT) {
           setErrorDialog("This username already exists");
         } else {
           setErrorDialog("Please try again later");
         }
       });
+
     if (res && res.status === STATUS_CODE_CREATED) {
       setSuccessDialog("Account successfully created");
       setIsSignupSuccess(true);
@@ -64,27 +71,81 @@ function SignupPage() {
         </Typography>
         <div className="textfields">
           <TextField
+            required
+            className="field"
             label="Username"
-            variant="outlined"
+            error={isUserInputTouched && username === ""}
+            variant="filled"
             value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            sx={{ marginBottom: "1rem" }}
-            autoFocus
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+            onBlur={() => {
+              setUserInputTouched(true);
+            }}
           />
+
+          <div
+            className={`error-msg ${
+              isUserInputTouched && username === "" ? "" : "hide"
+            }`}
+          >
+            • Username cannot be empty
+          </div>
+
           <TextField
+            required
+            className="field"
+            label="Email"
+            error={isEmailInputTouched && email === ""}
+            variant="filled"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            onBlur={() => {
+              setEmailInputTouched(true);
+            }}
+          />
+
+          <div
+            className={`error-msg ${
+              isEmailInputTouched && email === "" ? "" : "hide"
+            }`}
+          >
+            • Email cannot be empty
+          </div>
+
+          <TextField
+            required
+            className="field"
             label="Password"
-            variant="outlined"
+            error={isPasswordInputTouched && password === ""}
+            variant="filled"
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            sx={{ marginBottom: "2rem" }}
+            onBlur={() => {
+              setPasswordInputTouched(true);
+            }}
           />
+
+          <div
+            className={`error-msg ${
+              isPasswordInputTouched && password === "" ? "" : "hide"
+            }`}
+          >
+            • Password cannot be empty
+          </div>
         </div>
 
         <Box>
           <Button className="signup-btn" onClick={handleSignup}>
             Sign up
           </Button>
+        </Box>
+
+        <Box className="text-center">
+          Already have an account? <a href="./">Sign in.</a>
         </Box>
 
         <Dialog open={isDialogOpen} onClose={closeDialog}>
