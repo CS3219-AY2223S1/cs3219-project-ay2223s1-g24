95cd29adabebba4807c35fd9ecd5d96c8046a4a0
Darren <darrenhoon.ad@gmail.com>
2022-09-21 08:11:33 +0800

    the logo reverted so I guess someone screwed up

---
+++ frontend/src/components/Editor/Editor.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+// The following imports is for the theme.
+import "codemirror/lib/codemirror.css";
+import 'codemirror/theme/material.css'
+import "./editor.scss";
+
+import { Controlled as ControlledEditor } from 'react-codemirror2'
+import { Button } from "@mui/material";
+import MenuItem from "@mui/material/MenuItem";
+import ControlDropdown from "../ControlDropdown/ControlDropdown";
+
+export default function Editor(props) {
+
+  const {
+    setLanguage,
+    language,
+    value,
+    onChange
+  } = props
+
+  const modes = [
+    { name: "Javascript", code: "javascript" },
+    { name: "Python", code: "python" },
+  ];
+
+  const handleMode = (e) => {
+    setLanguage( e.target.value );
+  };
+
+  function handleChange(editor, data, value) {
+    onChange(value)
+  }
+
+  return (
+    <div className="editor-container">
+      <div className="editor-title">
+        <ControlDropdown
+          default={language}
+          options={modes}
+          handleDropdown={handleMode}
+        />
+        <button>Fullscreen</button>
+      </div>
+      <ControlledEditor 
+        onBeforeChange={handleChange}
+        value={value}
+        className="code-mirror-wrapper"
+        options={{
+            lineWrapping: true,
+            lint: true,
+            mode: language,
+            theme: 'material',
+            lineNumbers: true
+        }}
+      />
+    </div>
+  )
+}
---
+++ frontend/src/views/DashboardPage/CodeComponent.js
@@ -0,0 +1,80 @@
+import { Button } from "@mui/material";
+import React, { useState } from "react";
+import mockData from "assets/mockdata.json";
+import "./codeComponent.scss";
+<<<<<<< HEAD
+=======
+import Editor from "components/Editor/Editor";
+>>>>>>> 2babe3d28ed1eeb3bb2054266f91b4daa5744fd6
+
+function CodeComponent({ returnFunc }) {
+  const [javascript, setJavascript] = useState('');
+
+  return (
+    <div className="codeComponent">
+      <div className="left">
+        <div className="title">
+          {" "}
+          {mockData["id"]}. {mockData["title"]}{" "}
+        </div>
+        <div className="desc"> {mockData["desc"]} </div>
+        <div className="examples">
+          {mockData["examples"].map((example, idx) => (
+            <div>
+              <div className="example-header"> Example {idx + 1}: </div>
+              <div className="example">
+                <div>
+                  {" "}
+                  <span className="keyword"> Input: </span> {example[0]}
+                </div>
+                <div>
+                  {" "}
+                  <span className="keyword"> Output: </span> {example[1]}{" "}
+                </div>
+                {example[2] && (
+                  <div>
+                    {" "}
+                    <span className="keyword"> Explanation: </span> {example[2]}
+                  </div>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="constraints">
+          <div className="constraints-header"> Constraints: </div>
+          {mockData["constraints"].map((constraint) => (
+            <li className="constraint"> {constraint} </li>
+          ))}{" "}
+        </div>
+
+        <div className="return" onClick={() => returnFunc(0)}>
+          {" "}
+          Back{" "}
+        </div>
+      </div>
+<<<<<<< HEAD
+<<<<<<< HEAD
+      <div className="right"> Right</div>
+=======
+=======
+>>>>>>> 2babe3d28ed1eeb3bb2054266f91b4daa5744fd6
+
+      {/* To be changed */}
+      <div className="code-editor"> 
+        <Editor 
+          language="javascript" 
+          displayName="Javascript"
+          value={javascript}
+          onChange={setJavascript}
+        />
+      </div>
+<<<<<<< HEAD
+>>>>>>> 8c20e2a (Add a new page for the coding page, navigate to coding page after)
+=======
+>>>>>>> 2babe3d28ed1eeb3bb2054266f91b4daa5744fd6
+    </div>
+  );
+}
+
+export default CodeComponent;
