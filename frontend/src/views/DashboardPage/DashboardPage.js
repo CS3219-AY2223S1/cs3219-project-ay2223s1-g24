856bd90da94b7e1ae55f0f473208d8622584dcb3
yk Wei <ykwei7@gmail.com>
2022-09-26 11:41:39 +0800

    Fix bug for login

--- frontend/src/views/DashboardPage/DashboardPage.js
+++ frontend/src/views/DashboardPage/DashboardPage.js
@@ -30,7 +30,7 @@ import {
 } from "constants";
 import { USER_SERVICE_API_ENDPOINT } from "configs";
 import axios from "axios";
-import { decodeToken, isExpired } from "react-jwt";
+import { isExpired } from "react-jwt";
 
 function DashboardPage() {
   const [value, setValue] = useState("one");
@@ -68,13 +68,8 @@ function DashboardPage() {
   ]);
 
   useEffect(() => {
-    const decodedToken = decodeToken(cookies?.jwtToken);
-    if (
-      !cookies.jwtToken ||
-      decodedToken?.email !== cookies?.email ||
-      decodedToken?.name !== cookies?.name ||
-      isExpired(cookies.jwtToken)
-    ) {
+    // const decodedToken = decodeToken(cookies?.jwtToken);
+    if (!cookies.jwtToken || isExpired(cookies.jwtToken)) {
       removeCookie("name", { path: "/" });
       removeCookie("email", { path: "/" });
       removeCookie("jwtToken", { path: "/" });
