e46f08681660051267c790859395ffe666b4188c
yk Wei <ykwei7@gmail.com>
2022-09-29 22:10:07 +0800

    Add logo

--- frontend/src/components/CodeNavBar/CodeNavBar.js
+++ frontend/src/components/CodeNavBar/CodeNavBar.js
@@ -3,6 +3,7 @@ import Button from "@mui/material/Button";
 import "./codenavbar.scss";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import DashboardPage from "views/DashboardPage/DashboardPage";
+import mainLogo from "assets/logo.png";
 
 function CodeNavBar() {
   const navigate = useNavigate();
@@ -11,25 +12,34 @@ function CodeNavBar() {
   };
   return (
     <div className="code-navbar">
+      <div className="left">
+        <div className="img-container">
+          <img src={mainLogo} alt="main-logo" />
+        </div>
+      </div>
       {/*OnClick behaviour to be changed*/}
-      <Button
-        className="save-button"
-        onClick={() => console.log("Saved successfully!")}
-        color="success"
-        variant="contained"
-        size="small"
-      >
-        Save
-      </Button>
-      <Button
-        className="end-button"
-        onClick={navigateDashboard}
-        color="error"
-        variant="contained"
-        size="small"
-      >
-        End Session
-      </Button>
+      <div className="right">
+        <div className="buttons">
+          <Button
+            className="save-button"
+            onClick={() => console.log("Saved successfully!")}
+            color="success"
+            variant="contained"
+            size="small"
+          >
+            Save
+          </Button>
+          <Button
+            className="end-button"
+            onClick={navigateDashboard}
+            color="error"
+            variant="contained"
+            size="small"
+          >
+            End Session
+          </Button>
+        </div>
+      </div>
       <Routes>
         <Route path="/dashboard/*" element={<DashboardPage />} />
       </Routes>
