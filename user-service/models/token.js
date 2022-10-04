20446e958064db9de218054f6c25620980856893
yk Wei <ykwei7@gmail.com>
2022-10-04 20:04:17 +0800

    Add token verification schema to store verification tokens

--- user-service/models/token.js
+++ user-service/models/token.js
@@ -2,9 +2,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const tokenSchema = new Schema({
-  email: { type: String, required: true, unique: true },
+  userId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+    unique: true,
+  },
   token: { type: String, required: true },
   createdAt: { type: Date, default: Date.now, expires: 3600 },
 });
 
-module.exports = mongoose.model("token", tokenSchema);
+module.exports = mongoose.model("tokens", tokenSchema);
