f46dfb74eab36165ef436c008dcf7a8f5e800011
Darren <darrenhoon.ad@gmail.com>
2022-10-14 13:11:42 +0800

    change all endpoints to localhost

--- matching-service/server.js
+++ matching-service/server.js
@@ -17,8 +17,8 @@ app.get('/', (req, res) => {
 const httpServer = createServer(app)
 const io = new Server(httpServer, {
     cors: {
-        // origin: ["http://localhost:3000"]
-        origin: '*',
+        origin: ["http://localhost:3000"]
+        // origin: '*', 
     }
 })
 
