f77aae84cb888c95d21f8e76a0f2ac6008079688
David <67327422+xSaints19x@users.noreply.github.com>
2022-10-24 13:04:45 +0800

    Change config to use localhost endpoint
    - Commented out deployed api endpoints
    - For testing purposes

--- frontend/src/configs.js
+++ frontend/src/configs.js
@@ -7,10 +7,10 @@ export const URL_USER_SVC = URI_USER_SVC + PREFIX_USER_SVC;
 // export const USER_SERVICE_API_ENDPOINT = "http://user-service-dev.ap-southeast-1.elasticbeanstalk.com/api/users/";
 export const USER_SERVICE_API_ENDPOINT = "http://localhost:8088/api/users/";
 
-export const MATCHING_SERVICE_SOCKET_ENDPOINT =
-  "http://matching-service-dev.ap-southeast-1.elasticbeanstalk.com/";
-// export const MATCHING_SERVICE_SOCKET_ENDPOINT = "http://localhost:8080";
+// export const MATCHING_SERVICE_SOCKET_ENDPOINT =
+//   "http://matching-service-dev.ap-southeast-1.elasticbeanstalk.com/";
+export const MATCHING_SERVICE_SOCKET_ENDPOINT = "http://localhost:8080";
 
-export const COLLABORATION_SERVICE_SOCKET_ENDPOINT =
-  "http://collaboration-service-dev.ap-southeast-1.elasticbeanstalk.com/";
-// export const COLLABORATION_SERVICE_SOCKET_ENDPOINT = "http://localhost:8081";
+// export const COLLABORATION_SERVICE_SOCKET_ENDPOINT =
+//   "http://collaboration-service-dev.ap-southeast-1.elasticbeanstalk.com/";
+export const COLLABORATION_SERVICE_SOCKET_ENDPOINT = "http://localhost:8081";
